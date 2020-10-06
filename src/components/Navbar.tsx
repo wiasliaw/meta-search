926dee664a97901d5f81b2b3755c517430d34765
Wei Lee <leewei48750@gmail.com>
2020-10-07 06:25:49 +0800

    Refactor

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,79 +1,38 @@
 import React from 'react';
-import { InputBase } from '@material-ui/core';
-import { GitHub, Search } from '@material-ui/icons';
+import { FaGithub } from 'react-icons/fa';
+import styled from 'styled-components';
 
-interface IProp {
-  updateSearch: Function;
-}
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 0 5%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 72px;
+  z-index: 1000;
+  background-color: #364f6b;
+`;
 
-const Navbar: React.FC<IProp> = ({ updateSearch }) => {
-  const handleSearch = async (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    const request = await fetch(
-      `https://google-search3.p.rapidapi.com/api/v1/search/q=${parseInput()}&num=10`,
-      {
-        'method': 'GET',
-        headers: new Headers({
-          'x-rapidapi-host': 'google-search3.p.rapidapi.com',
-          'x-rapidapi-key': `${process.env.REACT_APP_API_KEY}`,
-        }),
-      },
-    );
-    if (request.status === 200) {
-      const data = await request.json();
-      updateSearch(data.results);
-    } else {
-      updateSearch(
-        {
-          answers: [],
-          image_results: [],
-          results: [
-            {
-              description: '',
-              title: 'Error',
-              link: '',
-            }
-          ],
-          total: 0,
-        }
-      );
-    }
-  }
+const Title = styled.h2`
+  color: #fff;
+`;
 
-  const parseInput = (): String => {
-    const inputText = document.querySelector<HTMLInputElement>('#search-input');
-    return (inputText?.value)?.replace(/[ ]/gi, '+') || '';
-  }
-
-  return (
-    <nav id="navbar" className="white">
-      <div className="center">
-        <h3>Meta Search</h3>
-      </div>
-      <div className="center">
-        <div id="search" className="center">
-          <div onClick={handleSearch}>
-            <Search />
-          </div>
-          <InputBase
-            id="search-input"
-            placeholder="Search..."
-            type="text"
-          />
-        </div>
-      </div>
-      <div className="center">
-        <a
-          href="https://github.com/wiasliaw77210/meta-search"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: '#fff' }}
-        >
-          <GitHub />
-        </a>
-      </div>
-    </nav>
-  );
-}
+const Navbar = () => (
+  <Nav>
+    <Title>Meta Search</Title>
+    <a
+      href="https://github.com/wiasliaw77210/meta-search"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <FaGithub
+        style={{ width: '35px', height: '35px', color: '#fff' }}
+      />
+    </a>
+  </Nav>
+);
 
 export default Navbar;
---
+++ src/typing.ts
@@ -0,0 +1,6 @@
+export interface IAPIResponse {
+  title: string,
+  link: string,
+  description: string,
+  type: 'google' | 'bing',
+}
