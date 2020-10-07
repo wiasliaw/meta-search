beba2e515e865b1c3212f9d7a9c0bf43ee2de57f
Wei Lee <leewei48750@gmail.com>
2020-10-07 16:36:31 +0800

    Updates

---
+++ precache-manifest.dc70130d11c6308a3d1459abdd18e6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb0803897e6ec901fe708ae6b34fc1a",
+    "url": "/meta-search/index.html"
+  },
+  {
+    "revision": "58f69ec54ec85f4902df",
+    "url": "/meta-search/static/css/main.90c7d047.chunk.css"
+  },
+  {
+    "revision": "1c1ad286f7930f600d2b",
+    "url": "/meta-search/static/js/2.436dd101.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meta-search/static/js/2.436dd101.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f69ec54ec85f4902df",
+    "url": "/meta-search/static/js/main.12144cdf.chunk.js"
+  },
+  {
+    "revision": "78835f2ddc7085df9832",
+    "url": "/meta-search/static/js/runtime-main.9316bb70.js"
+  }
+]);
--- src/typing.ts
+++
@@ -1,6 +0,0 @@
-export interface IAPIResponse {
-  title: string,
-  link: string,
-  description: string,
-  type: 'google' | 'bing',
-}
